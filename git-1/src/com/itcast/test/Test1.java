5dda14227a3b2354aecf2b0da57522e9270d99d3
admin <admin@itcast.com>
2019-06-09 20:18:10 +0800

    第二次把本地工程添加到git

--- git-1/src/com/itcast/test/Test1.java
+++ git-1/src/com/itcast/test/Test1.java
@@ -3,5 +3,7 @@ package com.itcast.test;
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("11111111111");
+
+        System.out.println("222222222222");
     }
 }
